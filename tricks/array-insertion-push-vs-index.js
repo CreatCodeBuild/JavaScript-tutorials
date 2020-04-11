fc2b4fba56e727ea275c88aa02254340c6a373be
CreatCodeBuild <CreatCodeBuild@github.com>
2020-04-11 00:09:54 -0700

    +

--- tricks/array-insertion-push-vs-index.js
+++ tricks/array-insertion-push-vs-index.js
@@ -1,31 +1,45 @@
+/*
+JavaScript 小技巧 1：push较之于键值对添加
+*/
+
 function time(f) {
     let t = new Date();
-    f();
+    f(arguments[1]);
     return new Date() - t;
 }
 
-function warmUp(f) { f() }
+function warmUp(f) { f(arguments[1]) }
 
-const RANGE = 50000000;
-
-function push() {
+function push(RANGE) {
     let a = [];
     for (let i = 0; i < RANGE; i++) {
         a.push(i*2);
     }
+    return a;
 }
 
-function index() {
+function index(RANGE) {
     let a = [];
+    // a.key = value
+    // a["key"] = value
+    // a[number] = value
     for (let i = 0; i < RANGE; i++) {
         a[i] = i*2;
     }
+    return a;
 }
 
-global.gc();
-warmUp(push);
-console.log(time(push));
+
+console.log(push(10));
+console.log(index(10));
+
+
+const RANGE = 50000000;
 
 global.gc();
-warmUp(index);
-console.log(time(index));
+warmUp(index, RANGE);
+console.log(time(index, RANGE));
+
+global.gc();
+warmUp(push, RANGE);
+console.log(time(push, RANGE));
